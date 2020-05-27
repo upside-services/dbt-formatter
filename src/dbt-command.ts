3d3782eda7634b8f2836506b657ffcc0142d24cc
Elliott O'Hara <elliott.ohara@getupside.com>
2020-05-27 10:54:21 -0500

    Squash this

--- src/dbt-command.ts
+++ src/dbt-command.ts
@@ -7,8 +7,8 @@ import { writer } from 'repl';
 
 program
   .option('-f, --file <file>', 'sql file to format')
-  .option('-d, --directory <directory>', 'The directory to recursively search.')
-  .option('-i, --ignore <pattern>', 'Glob friendly pattern(s) of files to ignore in directory', (pattern, previous) => previous.concat([pattern]), [])
+  .option('-d, --directory <directory>', 'The glob pattern to search for files.')
+  .option('-i, --ignore <pattern>', 'Glob friendly pattern(s) of files to ignore in directory. Can use multiple.', (pattern, previous) => previous.concat([pattern]), [])
   .option('-s', '--spaces <spaces>', 'number of spaces for indent. Defaults to 4.')
   .option('--upper', 'upercase reserved sql words')
   .option('--replace', 'overwrites the linted file(s).')
@@ -31,12 +31,12 @@ const do_work = (file: string) => {
   writer.write(formatted)
   
 }
-
+console.log(`Program Directory: ${program.directory}`)
 if(program.file){
   do_work(program.file);
 }else if (program.directory){
-  let match = `${program.directory}/**/*.sql`
-  let all_files = program.ignore == undefined? glob.sync(match): glob.sync(match, {ignore: program.ignore});
+  let search_pattern = program.directory;
+  let all_files = program.ignore == undefined? glob.sync(search_pattern): glob.sync(search_pattern, {ignore: program.ignore});
   all_files.forEach(do_work)
 }
 
